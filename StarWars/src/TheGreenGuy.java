4b9b7f3fb5950fffe9c22bb8d31e619a825f2c5d
Richard Poore <mrrichpoore@gmail.com>
2015-10-20 15:31:53 -0500

    Committed we are

---
+++ StarWars/src/TheGreenGuy.java
@@ -0,0 +1,12 @@
+
+public class TheGreenGuy {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Do or do not.");
+		System.out.println("There is no try.");
+		System.out.println("The Java is strong with this one");
+		
+	}
+
+}
